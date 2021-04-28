aaa3bd55fc006acbedc2675fab7743ef6d483eac
Laura-Oliveira <laura.regina.cp@gmail.com>
2021-04-27 22:10:22 -0300

    Ajustes no cadastro de usuário

--- app/src/main/java/com/empodera/Controller/RegisterUserActivity.java
+++ app/src/main/java/com/empodera/Controller/RegisterUserActivity.java
@@ -22,10 +22,10 @@ public class RegisterUserActivity extends AppCompatActivity
     /* Get firebase database instance */
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-    private RadioGroup user, worker;
-    private EditText name, surname, email, phone, password;
+   // private RadioGroup user, worker;
+    private EditText name, email, phone, password;
 
-    private String txt_type_user, txt_worker, txt_name, txt_surname, txt_email, txt_password, txt_phone;
+    private String txt_name, txt_email, txt_password, txt_phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,11 +33,10 @@ public class RegisterUserActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
 
-        name = findViewById(R.id.register_username_edit_text);
-        surname = findViewById(R.id.register_surname_edit_text);
-        email = findViewById(R.id.register_email_edit_text);
-        password = findViewById(R.id.register_password_edit_text);
-        phone = findViewById(R.id.register_phone_edit_text);
+        name = (EditText) findViewById(R.id.register_username_edit_text);
+        email = (EditText) findViewById(R.id.register_email_edit_text);
+        password = (EditText) findViewById(R.id.register_password_edit_text);
+        phone = (EditText) findViewById(R.id.register_phone_edit_text);
     }
 
     public void registerUser(View view)
@@ -47,15 +46,8 @@ public class RegisterUserActivity extends AppCompatActivity
         DatabaseReference databaseReferenceInstance = database.getReference();
         DatabaseReference databaseReference = databaseReferenceInstance.child("user");
 
-        /* VALIDATE IF RADIO BUTTON IS CHECKED
-        if(typeUser)
-        {
-        }
-        */
-
         /* Converting EditText type into String type */
         txt_name = name.getText().toString();
-        txt_surname = surname.getText().toString();
         txt_email = email.getText().toString();
         txt_password = password.getText().toString();
         txt_phone = phone.getText().toString();
@@ -65,7 +57,6 @@ public class RegisterUserActivity extends AppCompatActivity
 
         /* Setting data into ServiceApp class */
         userApp.setName(txt_name);
-        userApp.setSurname(txt_surname);
         userApp.setEmail(txt_email);
         userApp.setPhone(txt_phone);
         userApp.setPassword(txt_password);
@@ -76,9 +67,8 @@ public class RegisterUserActivity extends AppCompatActivity
         /* Data ransfered from object to Firebase database */
         databaseReference.child(userApp.getId()).setValue(userApp);
 
-        Intent activity = new Intent(RegisterUserActivity.this, SearchWorkerActivity.class);
+        Intent activity = new Intent(RegisterUserActivity.this, LoginActivity.class);
         startActivity(activity);
-        Toast.makeText(this,"Usuário Cadastrado com Sucesso",Toast.LENGTH_LONG).show();
-
+        Toast.makeText(this,"Usuário(a) Cadastrado com Sucesso!",Toast.LENGTH_LONG).show();
     }
 }
