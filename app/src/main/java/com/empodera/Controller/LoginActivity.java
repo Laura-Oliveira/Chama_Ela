85d387407e92c92d409b2d654b58c41d57b108c2
Laura-Oliveira <laura.regina.cp@gmail.com>
2023-03-08 11:50:04 -0300

    Implementacao de login temporario

--- app/src/main/java/com/empodera/Controller/LoginActivity.java
+++ app/src/main/java/com/empodera/Controller/LoginActivity.java
@@ -5,22 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.empodera.ListServiceActivity;
 import com.empodera.R;
 
 public class LoginActivity extends AppCompatActivity
 {
+
+    private EditText email, passaword;
+    private String txt_email, txt_password;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.input_login_email_edit_text);
+        passaword = (EditText) findViewById(R.id.input_login_password_edit_text);
+
+        txt_email = email.getText().toString();
+        txt_password = passaword.getText().toString();
     }
 
-    public void openRegisterUserPage(View view)
+    public void openListServicePage(View view)
     {
-        Intent activity = new Intent(LoginActivity.this, RegisterUserActivity.class);
-        startActivity(activity);
+//        if(txt_email == "laura" && txt_password == "1234")
+//        {
+            Intent activity = new Intent(LoginActivity.this, ListServiceActivity.class);
+            Toast.makeText(this,"Login",Toast.LENGTH_LONG).show();
+            startActivity(activity);
+//        }
+
     }
 }
---
+++ app/src/main/java/com/empodera/ListServiceActivity.java
@@ -0,0 +1,14 @@
+package com.empodera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_service);
+    }
+}
