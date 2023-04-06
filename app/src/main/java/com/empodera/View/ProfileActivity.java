d235d6d46bfb3614b6077854988552601b80e46e
Laura-Oliveira <laura.regina.cp@gmail.com>
2023-04-06 19:28:06 -0300

    Leitura de dados do banco firebase

--- app/src/main/java/com/empodera/View/ProfileActivity.java
+++ app/src/main/java/com/empodera/View/ProfileActivity.java
@@ -4,52 +4,80 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.PostProcessor;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.empodera.Model.ServiceApp;
 import com.empodera.R;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.firebase.auth.FirebaseAuthException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.core.Tag;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.gson.Gson;
 
-public class ProfileActivity extends AppCompatActivity {
- /*   private FirebaseAuthException auth;
-    private FirebaseDatabase database;
-    private DatabaseReference databaseReference = (DatabaseReference) database.getInstance().getReference();
-    ServiceApp serviceApp = new ServiceApp();
-    private ValueEventListener valueEventListener;
-
-    Intent intent = getIntent();*/
+public class ProfileActivity extends AppCompatActivity
+{
+    private DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-    }
 
-/*    TextView serviceNameText = (TextView) findViewById(R.id.profile_service_name);
-    TextView priceServiceText = (TextView) findViewById(R.id.profile_service_price);
-    TextView priceTicketText = (TextView) findViewById(R.id.profile_price_ticket);
+        Intent intent = getIntent();
+        TextView usernameText = findViewById(R.id.profile_user_name);
+//        TextView userEmailText = findViewById(R.id.profile_user_email);
+//        TextView serviceNameText = findViewById(R.id.profile_service_name);
+//        TextView priceServiceText = findViewById(R.id.profile_service_price);
+//        TextView priceTicketText = findViewById(R.id.profile_price_ticket);
 
-    String serviceName = intent.getStringExtra("serviceName");*/
-//    String priceService = String.valueOf((intent.putExtra("priceService", priceServiceText.getText().toString())));
-//    String priceTicket = String.valueOf(intent.putExtra("priceTicket", priceTicketText.getText().toString()));
+       // String username = intent.getStringExtra("name");
+//        String userEmail = intent.getStringExtra("email");
+//        String serviceName = intent.getStringExtra("serviceName");
+//        String priceService = String.valueOf((intent.putExtra("priceService", priceServiceText.getText().toString())));
+//        String priceTicket = String.valueOf(intent.putExtra("priceTicket", priceTicketText.getText().toString()));
 
-    //serviceNameText.setText(serviceName);
+        //firebaseDatabase.child("user").child(username).child("name");
+        String firebaseResult = String.valueOf(firebaseDatabase.child("user").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task)
+            {
+                if (task.isSuccessful())
+                {
+//                    for (DataSnapshot resultList : task.getResult().getChildren()) {
+//                        String username = resultList.child("user").getValue(String.class);
+//                       // testInputTxt.setText(passwordUser+"tatata");
+//                        usernameText.setText(username);
+//                        Toast.makeText(ProfileActivity.this, (CharSequence) resultList, Toast.LENGTH_SHORT).show();
+//                    }
+                   // List<String> listUsers = Collections.singletonList(String.valueOf(task.getResult().getValue(String.class)));
+//                   String username = String.valueOf(task.getResult().getValue());
+//                   usernameText.setText(String.valueOf(task.getResult().getValue()));
+//                    String usename = task.getResult().getValue(String.class);
+//                    usernameText.setText(usename);
+                    Log.d("firebase child value", String.valueOf(task.getResult().getValue()));
+                    Toast.makeText(ProfileActivity.this, String.valueOf(task.getResult().getValue()) , Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(ProfileActivity.this, "Error retriving data", Toast.LENGTH_SHORT).show();
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+            }
+        }));
+        usernameText.setText(firebaseResult);
+//        userEmailText.setText(userEmail);
+//        serviceNameText.setText(serviceName);
+//        priceServiceText.setText(priceService);
+//        priceTicketText.setText(priceTicket);
 
-//    FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
-//    databaseReference = (DatabaseReference) database.getInstance().getReference("service").addValueEventListener(new ValueEventListener() {
+   }
+}
+
+
+//   databaseReference = (DatabaseReference) database.getInstance().getReference("service").addValueEventListener(new ValueEventListener() {
 //                                       @Override
 //                                       public void onDataChange(DataSnapshot dataSnapshot) {
 //                                           ServiceApp serviceApp = dataSnapshot.getValue(ServiceApp.class);
@@ -62,6 +90,6 @@ public class ProfileActivity extends AppCompatActivity {
 //
 //        }
 //    });
-}
+
 
 
