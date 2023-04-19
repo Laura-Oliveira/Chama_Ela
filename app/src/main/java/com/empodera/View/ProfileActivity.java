06260ee90cc1928040f4ce4ede63b2c21202d8fc
Laura-Oliveira <laura.regina.cp@gmail.com>
2023-04-19 19:34:41 -0300

    Testes leitura de dados do firebase

--- app/src/main/java/com/empodera/View/ProfileActivity.java
+++ app/src/main/java/com/empodera/View/ProfileActivity.java
@@ -1,6 +1,5 @@
 package com.empodera.View;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,7 +9,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.empodera.R;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
@@ -26,48 +24,38 @@ public class ProfileActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        Intent intent = getIntent();
         TextView usernameText = findViewById(R.id.profile_user_name);
 //        TextView userEmailText = findViewById(R.id.profile_user_email);
 //        TextView serviceNameText = findViewById(R.id.profile_service_name);
 //        TextView priceServiceText = findViewById(R.id.profile_service_price);
 //        TextView priceTicketText = findViewById(R.id.profile_price_ticket);
 
-       // String username = intent.getStringExtra("name");
 //        String userEmail = intent.getStringExtra("email");
 //        String serviceName = intent.getStringExtra("serviceName");
 //        String priceService = String.valueOf((intent.putExtra("priceService", priceServiceText.getText().toString())));
 //        String priceTicket = String.valueOf(intent.putExtra("priceTicket", priceTicketText.getText().toString()));
 
-        //firebaseDatabase.child("user").child(username).child("name");
-        String firebaseResult = String.valueOf(firebaseDatabase.child("user").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task)
-            {
-                if (task.isSuccessful())
-                {
-//                    for (DataSnapshot resultList : task.getResult().getChildren()) {
-//                        String username = resultList.child("user").getValue(String.class);
-//                       // testInputTxt.setText(passwordUser+"tatata");
-//                        usernameText.setText(username);
-//                        Toast.makeText(ProfileActivity.this, (CharSequence) resultList, Toast.LENGTH_SHORT).show();
-//                    }
-                   // List<String> listUsers = Collections.singletonList(String.valueOf(task.getResult().getValue(String.class)));
-//                   String username = String.valueOf(task.getResult().getValue());
-//                   usernameText.setText(String.valueOf(task.getResult().getValue()));
-//                    String usename = task.getResult().getValue(String.class);
-//                    usernameText.setText(usename);
-                    Log.d("firebase child value", String.valueOf(task.getResult().getValue()));
-                    Toast.makeText(ProfileActivity.this, String.valueOf(task.getResult().getValue()) , Toast.LENGTH_LONG).show();
+
+
+        String firebaseResult = String.valueOf(firebaseDatabase.child("user").child("name").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                if (task.getResult().exists()) {
+                    //DataSnapshot dataSnapshot = task.getValue<String>();
+                    DataSnapshot dataSnapshot = task.getResult();
+                    String username = dataSnapshot.child("user").child("name").getValue(String.class);
+                    usernameText.setText(username);
+                    Log.d("usernameText Value", String.valueOf(usernameText));
+                   // Log.d("firebase child value", task.getResult().getValue(String.class));
+                    Toast.makeText(ProfileActivity.this, task.getResult().getKey(), Toast.LENGTH_LONG).show();
                 }
-                else
-                {
-                    Toast.makeText(ProfileActivity.this, "Error retriving data", Toast.LENGTH_SHORT).show();
-                    Log.e("firebase", "Error getting data", task.getException());
-                }
+            } else {
+                Toast.makeText(ProfileActivity.this, "Error retriving data", Toast.LENGTH_SHORT).show();
+                Log.e("firebase error", "Error getting data", task.getException());
             }
         }));
-        usernameText.setText(firebaseResult);
+       usernameText.setText(firebaseResult);/**/
+       Toast.makeText(ProfileActivity.this, firebaseResult, Toast.LENGTH_LONG).show();
+
 //        userEmailText.setText(userEmail);
 //        serviceNameText.setText(serviceName);
 //        priceServiceText.setText(priceService);
@@ -75,21 +63,5 @@ public class ProfileActivity extends AppCompatActivity
 
    }
 }
-
-
-//   databaseReference = (DatabaseReference) database.getInstance().getReference("service").addValueEventListener(new ValueEventListener() {
-//                                       @Override
-//                                       public void onDataChange(DataSnapshot dataSnapshot) {
-//                                           ServiceApp serviceApp = dataSnapshot.getValue(ServiceApp.class);
-//                                           Gson gson = new Gson();
-//                                           String reqJson = gson.toJson(serviceApp);
-//                                       }
-//
-//        @Override
-//        public void onCancelled(@NonNull DatabaseError error) {
-//
-//        }
-//    });
-
 
 
