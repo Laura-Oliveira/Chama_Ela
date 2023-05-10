5b087d367561748f7c2eedb915b65b2d3fb35d22
“Laura-Oliveira” <laura.regina.cp@gmail.com>
2023-05-10 11:28:21 -0300

    Correcao na leitura de dados do firebase

--- app/src/main/java/com/empodera/View/ProfileActivity.java
+++ app/src/main/java/com/empodera/View/ProfileActivity.java
@@ -1,67 +1,211 @@
 package com.empodera.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.empodera.Model.ServiceApp;
 import com.empodera.R;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class ProfileActivity extends AppCompatActivity
-{
+public class ProfileActivity extends AppCompatActivity {
     private DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference();
+    ServiceApp serviceApp = new ServiceApp();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         TextView usernameText = findViewById(R.id.profile_user_name);
-//        TextView userEmailText = findViewById(R.id.profile_user_email);
-//        TextView serviceNameText = findViewById(R.id.profile_service_name);
-//        TextView priceServiceText = findViewById(R.id.profile_service_price);
-//        TextView priceTicketText = findViewById(R.id.profile_price_ticket);
+        TextView userEmailText = findViewById(R.id.profile_user_email);
+        TextView userPhoneText = findViewById(R.id.profile_user_phone);
+        TextView serviceNameText = findViewById(R.id.profile_service_name);
+        TextView priceServiceText = findViewById(R.id.profile_service_price);
+        TextView priceTicketText = findViewById(R.id.profile_price_ticket);
 
-//        String userEmail = intent.getStringExtra("email");
-//        String serviceName = intent.getStringExtra("serviceName");
-//        String priceService = String.valueOf((intent.putExtra("priceService", priceServiceText.getText().toString())));
-//        String priceTicket = String.valueOf(intent.putExtra("priceTicket", priceTicketText.getText().toString()));
+        firebaseDatabase.child("user").child("-NPZUUObPJpwgCrNjbk9").child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists())
+                {
+                    String data = dataSnapshot.getValue(String.class);
 
-
-
-        String firebaseResult = String.valueOf(firebaseDatabase.child("user").child("name").get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                if (task.getResult().exists()) {
-                    //DataSnapshot dataSnapshot = task.getValue<String>();
-                    DataSnapshot dataSnapshot = task.getResult();
-                    String username = dataSnapshot.child("user").child("name").getValue(String.class);
-                    usernameText.setText(username);
-                    Log.d("usernameText Value", String.valueOf(usernameText));
-                   // Log.d("firebase child value", task.getResult().getValue(String.class));
-                    Toast.makeText(ProfileActivity.this, task.getResult().getKey(), Toast.LENGTH_LONG).show();
+                    if (data != null)
+                    {
+                        usernameText.setText(data);
+                    }
+                    else
+                    {
+                        usernameText.setText("String nula");
+                    }
                 }
-            } else {
-                Toast.makeText(ProfileActivity.this, "Error retriving data", Toast.LENGTH_SHORT).show();
-                Log.e("firebase error", "Error getting data", task.getException());
+                else
+                {
+                    usernameText.setText("Dados não encontrados");
+                }
             }
-        }));
-       usernameText.setText(firebaseResult);/**/
-       Toast.makeText(ProfileActivity.this, firebaseResult, Toast.LENGTH_LONG).show();
 
-//        userEmailText.setText(userEmail);
-//        serviceNameText.setText(serviceName);
-//        priceServiceText.setText(priceService);
-//        priceTicketText.setText(priceTicket);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+                Log.e("ProfileActivity", "Erro ao ler dados do banco de dados: " + databaseError.getMessage());
+            }
+        });
 
-   }
+     /*   firebaseDatabase.child("user").child("-NPZUUObPJpwgCrNjbk9").child("email").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists())
+                {
+                    String data = dataSnapshot.getValue(String.class);
+
+                    if (data != null)
+                    {
+                        userEmailText.setText(data);
+                    }
+                    else
+                    {
+                        usernameText.setText("String nula");
+                    }
+                }
+                else
+                {
+                    usernameText.setText("Dados não encontrados");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+                Log.e("ProfileActivity", "Erro ao ler dados do banco de dados: " + databaseError.getMessage());
+            }
+        });
+
+        firebaseDatabase.child("user").child("-NPZUUObPJpwgCrNjbk9").child("phone").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists())
+                {
+                    String data = dataSnapshot.getValue(String.class);
+
+                    if (data != null)
+                    {
+                        userPhoneText.setText(data);
+                    }
+                    else
+                    {
+                        usernameText.setText("String nula");
+                    }
+                }
+                else
+                {
+                    usernameText.setText("Dados não encontrados");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+                Log.e("ProfileActivity", "Erro ao ler dados do banco de dados: " + databaseError.getMessage());
+            }
+        });
+
+        firebaseDatabase.child("service").child("-NPdA9fJElXyZiDLNdbe").child("serviceName").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists())
+                {
+                    String data = dataSnapshot.getValue(String.class);
+
+                    if (data != null)
+                    {
+                        serviceNameText.setText(data);
+                    }
+                    else
+                    {
+                        usernameText.setText("String nula");
+                    }
+                }
+                else
+                {
+                    usernameText.setText("Dados não encontrados");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+                Log.e("ProfileActivity", "Erro ao ler dados do banco de dados: " + databaseError.getMessage());
+            }
+        });
+
+        firebaseDatabase.child("service").child("-NPdA9fJElXyZiDLNdbe").child("priceService").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists())
+                {
+                    String data = dataSnapshot.getValue(String.class);
+
+                    if (data != null)
+                    {
+                        priceServiceText.setText(data);
+                    }
+                    else
+                    {
+                        usernameText.setText("String nula");
+                    }
+                }
+                else
+                {
+                    usernameText.setText("Dados não encontrados");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+                Log.e("ProfileActivity", "Erro ao ler dados do banco de dados: " + databaseError.getMessage());
+            }
+        });
+
+        firebaseDatabase.child("service").child("-NPdA9fJElXyZiDLNdbe").child("priceTicket").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists())
+                {
+                    String data = dataSnapshot.getValue(String.class);
+
+                    if (data != null)
+                    {
+                        priceTicketText.setText(data);
+                    }
+                    else
+                    {
+                        usernameText.setText("String nula");
+                    }
+                }
+                else
+                {
+                    usernameText.setText("Dados não encontrados");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+                Log.e("ProfileActivity", "Erro ao ler dados do banco de dados: " + databaseError.getMessage());
+            }
+        }); */
+    }
 }
 
 
